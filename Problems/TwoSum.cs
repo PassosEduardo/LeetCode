1fa77b55bdb7f5a5fdb490d35134236d8ee9b600
PassosEduardo <eduardo.sepassos@gmail.com>
2025-01-29 21:56:27 -0300

    add two sum

---
+++ Problems/TwoSum.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Problems;
+public static class TwoSum
+{
+    public static int[] FindTwoSum(int[] nums, int target)
+    {
+        for (int fisrtNumberIndex = 0; fisrtNumberIndex < nums.Length; fisrtNumberIndex++)
+        {
+            for (int secondNumebrIndex = 1; secondNumebrIndex < nums.Length; secondNumebrIndex++)
+            {
+                if (nums[fisrtNumberIndex] + nums[secondNumebrIndex] == target)
+                {
+                    return [fisrtNumberIndex, secondNumebrIndex];
+                }
+            }            
+        }
+
+        throw new Exception();
+    }
+}
